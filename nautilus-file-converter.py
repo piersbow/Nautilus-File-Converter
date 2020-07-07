425a50b1bfccbb7e3aaa1d8f74c7942b1a5f2d84
Piers Bowater <58085555+piersbow@users.noreply.github.com>
2020-07-07 11:33:44 +0000

    Update nautilus-file-converter.py

--- nautilus-file-converter.py
+++ nautilus-file-converter.py
@@ -2,7 +2,7 @@
 # Nautilus File Converter 1.1.0
 # Copyright (C) 2020 Piers Bowater https://bowater.org/projects/nautilus-file-converter
 #
-# FNautilus File Converter is free software; you can redistribute it and/or modify
+# Nautilus File Converter is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Folder Color; if not, see http://www.gnu.org/licenses
+# along with Nautilus File Converter; if not, see http://www.gnu.org/licenses
 # for more information.
 
 import gi, subprocess, threading, os
